2595b21c7e3123a551f674e6485525013824c403
Maxim Borzenkov <mborzenkov-dev@yandex.ru>
2017-10-13 18:31:40 +0500

    Added new tests

--- test/sortings/SortingsTest.java
+++ test/sortings/SortingsTest.java
@@ -24,7 +24,7 @@ public class SortingsTest {
     /** Types of sortings. */
     private enum SortingTypes {
         ARRAYS_SORT("Built-in Arrays.sort()"), INSERTION("Insertion Sort"), MERGE("Merge Sort"), BUBBLE("Bubble Sort"),
-                SELECTION("Selection Sort");
+                SELECTION("Selection Sort"), QUICK("Quick Sort");
 
         /** String representation of sorting type. */
         private final String representation;
@@ -40,6 +40,10 @@ public class SortingsTest {
 
     /** Array of random ints from 0 to Integer.MAX_VALUE, probably not unique numbers. */
     private static final int[] RANDOM_ARRAY;
+    /** Array of ints from 0 to 10, numbers are repeating frequently. */
+    private static final int[] RANDOM_REPEATING_ARRAY;
+    /** Array of same numbers. */
+    private static final int[] SAME_NUMBERS_ARRAY;
     /** Array of ints from 0 to ARRAYS_SIZE - 1, all numbers are unique. */
     private static final int[] SORTED_ARRAY;
     /** Array of ints from ARRAYS_SIZE - 1 to 0, all numbers are unique. */
@@ -52,67 +56,68 @@ public class SortingsTest {
     // All types of testing arrays, initialized with values
     static {
         RANDOM_ARRAY = new int[ARRAYS_SIZE];
+        RANDOM_REPEATING_ARRAY = new int[ARRAYS_SIZE];
+        SAME_NUMBERS_ARRAY = new int[ARRAYS_SIZE];
         SORTED_ARRAY = new int[ARRAYS_SIZE];
         BACKWARDS_SORTED_ARRAY = new int[ARRAYS_SIZE];
         Random randomizer = new Random();
         for (int i = 0; i < ARRAYS_SIZE; i++) {
+            SAME_NUMBERS_ARRAY[i] = 1;
             SORTED_ARRAY[i] = i;
             BACKWARDS_SORTED_ARRAY[ARRAYS_SIZE - 1 - i] = i;
             RANDOM_ARRAY[i] = randomizer.nextInt();
+            RANDOM_REPEATING_ARRAY[i] = randomizer.nextInt(10);
         }
     }
 
     /** Testing built-in Arrays.sort() algorithm. */
     @Test
     public void testBuiltInArraysSort() {
-        testSortOnArray(SORTED_ARRAY, "SORTED_ARRAY", SortingTypes.ARRAYS_SORT);
-        testSortOnArray(RANDOM_ARRAY, "RANDOM_ARRAY", SortingTypes.ARRAYS_SORT);
-        testSortOnArray(BACKWARDS_SORTED_ARRAY, "BACKWARDS_SORTED_ARRAY", SortingTypes.ARRAYS_SORT);
-        testSortOnArray(EMPTY_ARRAY, "EMPTY_ARRAY", SortingTypes.ARRAYS_SORT);
-        testSortOnArray(SIZE1_ARRAY, "SIZE1_ARRAY", SortingTypes.ARRAYS_SORT);
+        testAllSorts(SortingTypes.ARRAYS_SORT);
     }
 
     /** Testing insertion sort algorithm. */
     @Test
     public void testInsertionSort() {
-        testSortOnArray(SORTED_ARRAY, "SORTED_ARRAY", SortingTypes.INSERTION);
-        testSortOnArray(RANDOM_ARRAY, "RANDOM_ARRAY", SortingTypes.INSERTION);
-        testSortOnArray(BACKWARDS_SORTED_ARRAY, "BACKWARDS_SORTED_ARRAY", SortingTypes.INSERTION);
-        testSortOnArray(EMPTY_ARRAY, "EMPTY_ARRAY", SortingTypes.INSERTION);
-        testSortOnArray(SIZE1_ARRAY, "SIZE1_ARRAY", SortingTypes.INSERTION);
+        testAllSorts(SortingTypes.INSERTION);
     }
 
     /** Testing merge sort algorithm. */
     @Test
     public void testMergeSort() {
-        testSortOnArray(SORTED_ARRAY, "SORTED_ARRAY", SortingTypes.MERGE);
-        testSortOnArray(RANDOM_ARRAY, "RANDOM_ARRAY", SortingTypes.MERGE);
-        testSortOnArray(BACKWARDS_SORTED_ARRAY, "BACKWARDS_SORTED_ARRAY", SortingTypes.MERGE);
-        testSortOnArray(EMPTY_ARRAY, "EMPTY_ARRAY", SortingTypes.MERGE);
-        testSortOnArray(SIZE1_ARRAY, "SIZE1_ARRAY", SortingTypes.MERGE);
+        testAllSorts(SortingTypes.MERGE);
     }
 
     /** Testing selection sort algorithm. */
     @Test // this test is long
     @SuppressWarnings("unused")
     public void testSelectionSort() {
-        testSortOnArray(SORTED_ARRAY, "SORTED_ARRAY", SortingTypes.SELECTION);
-        testSortOnArray(RANDOM_ARRAY, "RANDOM_ARRAY", SortingTypes.SELECTION);
-        testSortOnArray(BACKWARDS_SORTED_ARRAY, "BACKWARDS_SORTED_ARRAY", SortingTypes.SELECTION);
-        testSortOnArray(EMPTY_ARRAY, "EMPTY_ARRAY", SortingTypes.SELECTION);
-        testSortOnArray(SIZE1_ARRAY, "SIZE1_ARRAY", SortingTypes.SELECTION);
+        testAllSorts(SortingTypes.SELECTION);
     }
 
     /** Testing bubble sort algorithm. */
     @Test // this test is long
     @SuppressWarnings("unused")
     public void testBubbleSort() {
-        testSortOnArray(SORTED_ARRAY, "SORTED_ARRAY", SortingTypes.BUBBLE);
-        testSortOnArray(RANDOM_ARRAY, "RANDOM_ARRAY", SortingTypes.BUBBLE);
-        testSortOnArray(BACKWARDS_SORTED_ARRAY, "BACKWARDS_SORTED_ARRAY", SortingTypes.BUBBLE);
-        testSortOnArray(EMPTY_ARRAY, "EMPTY_ARRAY", SortingTypes.BUBBLE);
-        testSortOnArray(SIZE1_ARRAY, "SIZE1_ARRAY", SortingTypes.BUBBLE);
+        testAllSorts(SortingTypes.BUBBLE);
     }
+
+    /** Testing quick sort algorithm. */
+    @Test
+    public void testQuickSort() {
+        testAllSorts(SortingTypes.QUICK);
+    }
+
+    private void testAllSorts(SortingTypes sortingType) {
+        testSortOnArray(SORTED_ARRAY, "SORTED_ARRAY", sortingType);
+        testSortOnArray(SAME_NUMBERS_ARRAY, "SAME_NUM_ARRAY", sortingType);
+        testSortOnArray(RANDOM_ARRAY, "RANDOM_ARRAY", sortingType);
+        testSortOnArray(RANDOM_REPEATING_ARRAY, "RANDOM_REP_ARRAY", sortingType);
+        testSortOnArray(BACKWARDS_SORTED_ARRAY, "BACKWARDS_SORTED_ARRAY", sortingType);
+        testSortOnArray(EMPTY_ARRAY, "EMPTY_ARRAY", sortingType);
+        testSortOnArray(SIZE1_ARRAY, "SIZE1_ARRAY", sortingType);
+    }
+
 
     /** Utility method for all tests.
      * Copy inputArray, call sorting method, print time, test result.
@@ -138,6 +143,9 @@ public class SortingsTest {
                 break;
             case BUBBLE:
                 BubbleSort.sort(array);
+                break;
+            case QUICK:
+                QuickSort.sort(array);
                 break;
             default:
                 throw new RuntimeException("unknown sorting type");
